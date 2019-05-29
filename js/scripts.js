4c9cae0f8f3f46604af15348b348a8a6f5ccabd6
Maurício Ackermann <mabiancoac@gmail.com>
2019-05-29 18:38:46 -0300

    ajustes solicitados pelo Rafael Baptista

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,52 @@
 let videosArr = [];
 let videosCounter = 0;
 const VIDEOS_NUMBER = $(window).width() > 1000 ? 9 : 8;
+let modalActive = false;
+let currentVideoId;
 $(".videos").on("click", '.video',function(){
   let id = $(this).data("id");
-  $(".lightbox .video").html(videosArr[id].embed);
-  $(".lightbox .video iframe").attr("width", 640)
-  $(".lightbox .video iframe").attr("height", 360)
-  $(".lightbox").show();
+  renderVideo(id);
 });
+
+
+function renderVideo(id){
+  $(".lightbox").hide();
+  $(".lightbox .video").html("");
+  $(".lightbox .video").html(videosArr[id].embed);
+  $(".lightbox .video").append("<p>"+ videosArr[id].description +"</p>")
+  $(".lightbox .video iframe").attr("width", 768)
+  $(".lightbox .video iframe").attr("height", 432)
+  $(".lightbox").show();
+  modalActive = true;
+  currentVideoId = id;
+}
+
+
+
 $(document).ready(function(){
+
+  $("body").keydown(function(e){
+    if(modalActive){
+      if(e.keyCode == 37){
+        let videoId = currentVideoId == 0 ? videosArr.length - 1 : currentVideoId - 1;
+        renderVideo(videoId);
+      }else if(e.keyCode == 39){
+        let videoId = currentVideoId < videosArr.length - 1 ? currentVideoId + 1 : 0;
+        renderVideo(videoId);
+      }
+    }
+  });
+
+  $(document).mouseup(function(e)
+    {
+    var container = $(".lightbox .video");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+    {
+      closeModal();
+    }
+    });
   $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
@@ -34,13 +72,16 @@ $(document).ready(function(){
     if (e.key === "Escape") { // escape key maps to keycode `27`
       closeModal();
     }
+
   });
+
 });
 
 
 function closeModal(){
   $(".lightbox .video").html("");
   $(".lightbox").hide();
+  modalActive = false;
 }
 
 async function getVideosIds(){
@@ -90,7 +131,7 @@ function printVideos(array){
   videosCounter = videosCounter + VIDEOS_NUMBER > videosArr.length ? videosArr.length : videosCounter + VIDEOS_NUMBER
   let videos = videosArr.slice(0, videosCounter)
   videos.map(function(video, index){
-    html += '<div class="video" data-id="'+ index + '" ><img title="'+ video.title +'" src="'+ video.thumb +'"></div>';
+    html += '<div class="video" data-id="'+ index + '" ><div class="overlay"><p>'+ video.title +'</p></div><img title="'+ video.title +'" src="'+ video.thumb +'"></div>';
   })
   $(".video-content").html(html);
   if(videosCounter == videosArr.length){
